6568e4ff014707b97c13feba1146ad3ab45d89be
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-06 11:49:43 -0500

    adding table user and the db context to connect to the database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
