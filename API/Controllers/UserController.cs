bf4b794a5d068d6c19e69b72b67c75cc6632d75b
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-10 23:01:05 -0500

    adding endpoint to sign up users

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,7 +1,10 @@
+using System.Security.Cryptography;
+using System.Text;
 using Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Models.DTOs;
 using Models.Entities;
 
 namespace API.Controllers
@@ -28,6 +31,30 @@ namespace API.Controllers
         {
             var user = await _db.Users.FindAsync(id);
             return Ok(user);
+        }
+
+        [HttpPost("sign-up")] // POST: api/user/registro
+        public async Task<ActionResult<User>> SignUp(SignUpDto signUpDto)
+        {
+            if (await UserExist(signUpDto.Username)) return BadRequest("Username already exist");
+
+            using var hmac = new HMACSHA512();
+            var user = new User
+            {
+                Username = signUpDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(signUpDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExist(string username)
+        {
+            return await _db.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower());
         }
     }
 }
