d766fce00d8fbdfea332a427f9f7015599889197
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-10 23:07:37 -0500

    making attributes of sign-up dto required

--- Models/DTOs/SignUpDto.cs
+++ Models/DTOs/SignUpDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models.DTOs;
 
 public class SignUpDto
 {
+    [Required(ErrorMessage = "The username is required")]
     public string Username { get; set; }
 
+    [Required(ErrorMessage = "The username is required")]
     public string Password { get; set; }
 }
