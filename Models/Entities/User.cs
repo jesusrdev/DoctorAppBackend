8b9e581590fd846bcbb0b3ba42a7aebf705afaa0
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-10 22:40:28 -0500

    adding password attributes and make the migration

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -3,5 +3,11 @@ namespace Models.Entities;
 public class User
 {
     public int Id { get; set; }
+    
     public string Username { get; set; }
+
+    public byte[] PasswordHash { get; set; }
+    
+    public byte[] PasswordSalt { get; set; }
+    
 }
