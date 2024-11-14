a7ee6b3321b232bb67bb9f7881040aa3deac3f6a
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-14 17:33:06 -0500

    adding the api response

---
+++ Models/DTO/ApiResponse.cs
@@ -0,0 +1,14 @@
+using System.Net;
+
+namespace Models.DTO;
+
+public class ApiResponse
+{
+    public HttpStatusCode StatusCode { get; set; }  //  200, 400, 500
+    
+    public bool isSuccessful { get; set; }
+
+    public string Message { get; set; }
+
+    public object Result { get; set; }  //  List, Entity
+}
